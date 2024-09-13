a0bb682f5aa45194231471d6e0c9015f6c0fe29c
atul7970 <a6394tul@gmail.com>
2024-09-13 16:40:03 +0530

    creating launches route

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
  const express = require('express');
- const planetsRouter=require('./routes/planets/planets.router');
  const cors = require('cors');
  const path = require('path');
  const morgan = require('morgan');
 
+ const planetsRouter=require('./routes/planets/planets.router');
+ const launchesRouter=require('./routes/launches/launches.router');
 
  const app = express();
 
@@ -12,8 +13,11 @@
     origin: 'http://localhost:3000',
   }));
  app.use(planetsRouter);
+ app.use(launchesRouter);
  app.use(express.static(path.join(__dirname, '..', 'public')));
  app.use(morgan('combined'));
+
+ 
 // app.use('/v1', api);
 
 app.get('/*', (req, res) => {
