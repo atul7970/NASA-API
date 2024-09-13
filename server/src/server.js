a493a6e672072530db01486971679374edd53d58
atul7970 <a6394tul@gmail.com>
2024-09-13 12:27:49 +0530

    feteched planets data

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const app = require('./app');
 // const { mongoConnect } = require('./services/mongo');
-// const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require('./models/planets.model');
 // const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 async function startServer() {
 //   await mongoConnect();
-//   await loadPlanetsData();
+  await loadPlanetsData();
 //   await loadLaunchData();
   
   server.listen(PORT, () => {
