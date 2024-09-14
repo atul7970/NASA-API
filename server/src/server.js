5d4f40e71a03bb16285da4b41e2980d994c484d9
atul7970 <a6394tul@gmail.com>
2024-09-14 14:00:19 +0530

    added database

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const mongoose = require('mongoose');
 // require('dotenv').config();
 
 const app = require('./app');
@@ -7,11 +7,18 @@ const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 // const { loadLaunchData } = require('./models/launches.model');
 
-const PORT = process.env.PORT || 8000;
-
+const PORT = process.env.PORT || 9000;
+const MONGO_URL = 'mongodb+srv://atul:atul1234@cluster1.6rpq1tr.mongodb.net/NASA_API?retryWrites=true&w=majority';
 const server = http.createServer(app);
+mongoose.connection.once('open', ()=>{
+  console.log('MongoDB connection ready!');
+})
 
+mongoose.connection.on('error', (err)=>{
+  console.error(err);
+})
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
 //   await mongoConnect();
   await loadPlanetsData();
 //   await loadLaunchData();
